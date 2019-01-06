71764401562cfb1185b7485d028d88e3d6091ad1
yanjing01 <yanjing01@fangdd.com>
2019-01-06 15:27:46 +0800

    add sell sort

--- java algorithm/sort/InsertSort.java
+++ java algorithm/sort/InsertSort.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
 public class InsertSort {
 
     /**
-     * 插入排序
+     * 直接插入排序
      * @param arr
      */
     public static void insertSort(int[] arr) {
@@ -32,9 +32,28 @@ public class InsertSort {
         System.out.println(Arrays.toString(arr));
     }
 
+    public static void shellSort(int[] arr) {
+        System.out.println(Arrays.toString(arr));
+
+        int gap = arr.length / 2;
+        while (gap > 0) {
+            for (int i = gap; i < arr.length; i++) {
+                int temp = arr[i];
+                int preIndex = i - gap;
+                while (preIndex >= 0 && arr[preIndex] > temp) {
+                    arr[preIndex + gap] = arr[preIndex];
+                    preIndex -= gap;
+                }
+                arr[preIndex + gap] = temp;
+            }
+            gap /= 2;
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+
     public static void main(String[] args) {
         int[] arr = {9, 8, 7, 6, 5, 4, 3, 2, 1};
-        insertSort(arr);
+        shellSort(arr);
     }
 }
-
